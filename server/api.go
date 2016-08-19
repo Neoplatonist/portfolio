ea9fdc48dcc3a78ac971029e42704b49a6147065
Neoplatonist <mr.neoplatonist@gmail.com>
2016-08-19 21:27:55 +0900

    Added example mongodb api

--- server/api.go
+++ server/api.go
@@ -3,6 +3,9 @@ package main
 import (
 	"time"
 
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
 	"gopkg.in/labstack/echo.v1"
 )
 
@@ -11,9 +14,20 @@ import (
 // your app as you wish.
 type API struct{}
 
+// Post defines data for mongodb
+type Post struct {
+	ID         bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Title      string        `json:"title"`
+	Body       string        `json:"body"`
+	Author     string        `json:"author"`
+	Created    time.Time     `json:"created"`
+	LastEdited time.Time     `json:"lastEdited"`
+}
+
 // Bind attaches api routes
 func (api *API) Bind(group *echo.Group) {
 	group.Get("/v1/conf", api.ConfHandler)
+	group.Get("/v1/posts", api.PostsRoute)
 }
 
 // ConfHandler handle the app config, for example
@@ -22,4 +36,22 @@ func (api *API) ConfHandler(c *echo.Context) error {
 	<-time.After(time.Millisecond * 500)
 	c.JSON(200, app.Conf.Root)
 	return nil
+}
+
+// PostsRoute mongodb API example
+func (api *API) PostsRoute(c *echo.Context) error {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	result := make([]Post, 0, 10)
+	if err := session.DB("go").C("post").Find(nil).All(&result); err != nil {
+		return err
+	}
+
+	// mPost, _ := bson.Marshal(&result)
+
+	return c.JSON(200, result)
 }
